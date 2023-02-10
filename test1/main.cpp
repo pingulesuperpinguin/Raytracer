72e7f0bb4f5b7ab2ce29d99d25607eca26da809f
Quentin Lavigne <quentin.lavigne@thermofisher.com>
2023-02-10 08:09:33 +0100

    [Clean] Fix indent.

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,15 +1,19 @@
 #include <SFML\Graphics.hpp>
 
 
-int main() {
+int main()
+{
     sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Hello World SFML Window");
 
-    while (sfmlWin.isOpen()) {
+    while (sfmlWin.isOpen())
+    {
 
         sf::Event e;
-        while (sfmlWin.pollEvent(e)) {
+        while (sfmlWin.pollEvent(e))
+        {
 
-            switch (e.type) {
+            switch (e.type)
+            {
             case sf::Event::EventType::Closed:
                 sfmlWin.close();
                 break;
