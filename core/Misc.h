d4f0898bcfc4451c25270baec61e558be6565d5d
Quentin Lavigne <quentin.lavigne@thermofisher.com>
2023-02-10 08:10:59 +0100

    [Enh] Add an IColorizer method getColor and IFlatColorizer class.

--- core/Misc.h
+++ core/Misc.h
@@ -97,7 +97,21 @@ template<typename T>
 class IColorizer
 {
 public:
+	virtual Color<T> getColor() = 0;
 	virtual ~IColorizer() = default;
+};
+
+template<typename T>
+class FlatColorizer : public IColorizer<T>
+{
+public:
+	FlatColorizer(const Color<T>& color) : m_color(color)
+	{}
+
+	virtual Color<T> getColor() { return m_color; }
+
+private:
+	Color<T> m_color;
 };
 
 template<typename T>
